7f66904c36d96ba00f388fa5c3873d15cfc3a070
nugaon <toth.viktor.levente@gmail.com>
2021-03-03 19:54:56 +0100

    feat: init popup page with changeable Bee API URL functionality

---
+++ src/popup-page/modules/app.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { getItem, StoreObserver, setItem } from '../../utils/storage'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props {}
+interface State {
+  beeApiUrl: string
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = { beeApiUrl: 'http://localhost:1633' }
+    this.asyncInit()
+
+    // bindings
+    this.handleBzzApiUrlChange = this.handleBzzApiUrlChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleBzzApiUrlChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({ beeApiUrl: event.target.value })
+  }
+
+  async handleSubmit(event: React.FormEvent<HTMLElement>): Promise<void> {
+    event.preventDefault()
+    await setItem('beeApiUrl', this.state.beeApiUrl)
+  }
+
+  async asyncInit(): Promise<void> {
+    this.setState({ beeApiUrl: await getItem('beeApiUrl') })
+  }
+
+  render(): JSX.Element {
+    return (
+      <form id="form-bee-api-url-change" onSubmit={this.handleSubmit}>
+        <label>
+          Bee node API address:
+          <input type="text" value={this.state.beeApiUrl} onChange={this.handleBzzApiUrlChange} />
+        </label>
+        <input type="submit" value="Change" />
+      </form>
+    )
+  }
+}
+
+export default App
