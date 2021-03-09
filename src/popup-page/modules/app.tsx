0d9121f3d4d3a4d7a98ed136866468aad3592ce5
nugaon <toth.viktor.levente@gmail.com>
2021-03-09 10:31:51 +0100

    refactor: using own state instead of direct globalState dispatch

--- src/popup-page/modules/app.tsx
+++ src/popup-page/modules/app.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { getItem, setItem } from '../../utils/storage'
 import { GlobalContext } from '../context/global'
 
 export function App(): JSX.Element {
   const globalStateContext = useContext(GlobalContext)
-  const { dispatch, state: globalState } = globalStateContext
+  const { dispatch: changeGlobalState, state: globalState } = globalStateContext
+  const [beeApiUrl, setBeeApiUrl] = useState(globalState.beeApiUrl)
 
   const asyncInit = async (): Promise<void> => {
-    const beeApiUrl = await getItem('beeApiUrl')
+    const storedBeeApiUrl = await getItem('beeApiUrl')
 
-    if (beeApiUrl) dispatch({ type: 'BEE_API_URL_CHANGE', newValue: beeApiUrl })
+    if (storedBeeApiUrl) {
+      changeGlobalState({ type: 'BEE_API_URL_CHANGE', newValue: storedBeeApiUrl })
+      setBeeApiUrl(storedBeeApiUrl)
+    }
   }
 
   useEffect(() => {
@@ -18,18 +22,15 @@ export function App(): JSX.Element {
 
   const handleSubmit = async (event: React.FormEvent<HTMLElement>): Promise<void> => {
     event.preventDefault()
-    await setItem('beeApiUrl', globalState.beeApiUrl)
-  }
-
-  const handleBzzApiUrlChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    dispatch({ type: 'BEE_API_URL_CHANGE', newValue: event.target.value })
+    await setItem('beeApiUrl', beeApiUrl)
+    changeGlobalState({ type: 'BEE_API_URL_CHANGE', newValue: beeApiUrl })
   }
 
   return (
     <form id="form-bee-api-url-change" onSubmit={handleSubmit}>
       <label>
         Bee node API address:
-        <input type="text" value={globalState.beeApiUrl} onChange={handleBzzApiUrlChange} />
+        <input type="text" value={beeApiUrl} onChange={e => setBeeApiUrl(e.target.value)} />
       </label>
       <input type="submit" value="Change" />
     </form>
