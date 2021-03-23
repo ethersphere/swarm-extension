9ca0f53ed6d2c72934f21d54c87c90b23dcd02cf
nugaon <toth.viktor.levente@gmail.com>
2021-03-23 17:15:17 +0100

    feat: background web2helper listener

--- src/background/feeder/web2-helper.feeder.ts
+++ src/background/feeder/web2-helper.feeder.ts
@@ -1,4 +1,4 @@
-import { ResponseMessageFormat, SendMessageFormat } from '../../utils/message/message-handler'
+import { ResponseMessageFormat, InterceptorReqMessageFormat } from '../../utils/message/message-handler'
 import { BeeApiListener } from '../listener/bee-api.listener'
 
 export class Web2HelperFeeder {
@@ -8,15 +8,17 @@ export class Web2HelperFeeder {
   serveEvents(): void {
     console.log('Register Web2HelperFeeder event listeners...')
 
-    chrome.runtime.onMessage.addListener((message: SendMessageFormat<string>) => {
+    chrome.runtime.onMessage.addListener((message: InterceptorReqMessageFormat<string>, sender, sendResponse) => {
+      console.log('Web2HelperFeeder beeApiUrl got aimed message from content script', message)
+
       if (message.key === 'beeApiUrl') {
         const response: ResponseMessageFormat = {
-          key: 'beeApiUrl',
+          key: message.key,
+          sender: 'background',
+          target: 'content',
           answer: this.beeApiListener.beeApiUrl,
         }
-        console.log('beeApiUrlRequest', response)
-
-        return Promise.resolve(response)
+        sendResponse(response)
       }
     })
   }
