2debba56acf4d5b0a438386856504ad2a6ed5b0c
nugaon <toth.viktor.levente@gmail.com>
2021-03-23 17:14:39 +0100

    feat: messaging structure

--- src/utils/message/message-handler.ts
+++ src/utils/message/message-handler.ts
@@ -1,18 +1,48 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export type SendMessageFormat<T = undefined> = {
+type BaseMessageFormat = {
+  /** What actually is requested */
   key: string
+}
+
+/** Used message type where window.postMessage call happens */
+export type BroadcastMessageFormat = BaseMessageFormat & {
+  eventId: string
+}
+
+/** Inpage script -> Content script */
+export type InpageReqMessageFormat<T = undefined> = BroadcastMessageFormat & {
+  sender: 'inpage'
+  target: 'content'
   payload?: T
 }
 
-export type ResponseMessageFormat<T = string> = {
-  key: string
+/** Content script -> Background script */
+export type InterceptorReqMessageFormat<T = undefined> = BaseMessageFormat & {
+  sender: 'content'
+  target: 'background'
+  payload?: T
+}
+
+/** Content script -> inpage script */
+export type InterceptorResMessageFormat<T = undefined> = BroadcastMessageFormat & {
+  sender: 'content'
+  target: 'inpage'
   answer?: T
   error?: string
 }
 
-export type ResponseMessage<T = string> = {
+/** Background script -> Content script */
+export type ResponseMessageFormat<T = string> = BaseMessageFormat & {
+  sender: 'background'
+  target: 'content'
+  answer?: T
+  error?: string
+}
+
+/** Background script -> Content script with answer */
+export type ResponseWithMessage<T = string> = {
   [Property in keyof ResponseMessageFormat<T> as Exclude<Property, 'error'>]-?: ResponseMessageFormat<T>[Property]
 }
 
@@ -20,11 +50,11 @@ export type EventReturnType<T extends (...args: any[]) => any> = T extends (...a
   ? Promise<R>
   : never
 
-export interface EventConsumer<Events extends { [eventName: string]: (...args: any[]) => Promise<any> }> {
+export interface EventInterceptor<Events extends { [eventName: string]: (...args: any[]) => Promise<any> }> {
   callEvent<E extends keyof Events>(eventName: E, ...args: Parameters<Events[E]>): EventReturnType<Events[E]>
 }
 
-export function deserializeResponseMessage<T>(message: ResponseMessageFormat<T>): ResponseMessage<T> {
+export function deserializeResponseMessage<T>(message: ResponseMessageFormat<T>): ResponseWithMessage<T> {
   if (message.error) {
     throw new Error(message.error)
   }
@@ -36,5 +66,7 @@ export function deserializeResponseMessage<T>(message: ResponseMessageFormat<T>)
   return {
     key: message.key,
     answer: message.answer,
+    target: 'content',
+    sender: 'background',
   }
 }
--- src/utils/message/web2-helper/web2-helper.message.ts
+++ src/utils/message/web2-helper/web2-helper.message.ts
@@ -1,5 +1,5 @@
 // Define your message types like that:
 // Key: Message name; Value: Feeder functions emit
-export type Web2HelperMessage = {
+export interface IWeb2HelperMessage {
   beeApiUrl: () => Promise<string>
 }
