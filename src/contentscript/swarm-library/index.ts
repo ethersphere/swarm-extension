98ccaa0ee024a957dce08730a8b334b5192579d1
nugaon <toth.viktor.levente@gmail.com>
2021-07-01 18:55:24 +0200

    feat(inpage): define localStorage under window.swarm

--- src/contentscript/swarm-library/index.ts
+++ src/contentscript/swarm-library/index.ts
@@ -1,7 +1,9 @@
-import { web2HelperContent } from '../web2-helper.content'
+import { localStorage } from './local-storage'
+import { web2HelperContent } from './web2-helper.content'
 
 window.swarm = {
   web2Helper: web2HelperContent,
+  localStorage,
 }
 
 console.log('window.swarm has been inited')
