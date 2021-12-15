c7708996fffc556d6ccc8924f15a316d05a9b390
nugaon <toth.viktor.levente@gmail.com>
2021-12-15 17:56:48 +0100

    feat: add global context to handle web2OriginEnabled

--- src/popup-page/context/global.tsx
+++ src/popup-page/context/global.tsx
@@ -18,6 +18,7 @@ interface State {
   beeDebugApiUrl: string
   postageBatchId: string | null
   globalPostageBatchEnabled: boolean
+  web2OriginEnabled: boolean
 }
 
 interface ActionBeeUrlSave {
@@ -39,12 +40,22 @@ interface ActionPostageBatchSave {
   newValue: string | null
 }
 
+interface ActionWeb2OriginEnableSave {
+  type: 'WEB2_ORIGIN_ENABLED_SAVE'
+  newValue: boolean
+}
+
 /**
  * The actions types are either ending with `_SAVE` or `_CHANGE`.
  *
  * The former saves the value into the localStorage, the latter only changes the globalState in the React app.
  */
-type Action = ActionPostageBatchSave | ActionBeeUrlSave | ActionGlobalPostageBatchEnabledSave | ActionBeeDebugUrlSave
+type Action =
+  | ActionPostageBatchSave
+  | ActionBeeUrlSave
+  | ActionGlobalPostageBatchEnabledSave
+  | ActionBeeDebugUrlSave
+  | ActionWeb2OriginEnableSave
 
 interface ContextValue {
   state: State
@@ -55,6 +66,9 @@ async function localStoreDispatch(action: Action): Promise<void> {
   switch (action.type) {
     case 'BEE_API_URL_SAVE':
       await setItem('beeApiUrl', action.newValue)
+      break
+    case 'WEB2_ORIGIN_ENABLED_SAVE':
+      await setItem('web2OriginEnabled', action.newValue)
       break
     case 'GLOBAL_POSTAGE_BATCH_SAVE':
       await setItem('globalPostageBatch', action.newValue)
@@ -75,6 +89,7 @@ const initialState: State = {
   beeDebugApiUrl: 'http://localhost:1635',
   postageBatchId: null,
   globalPostageBatchEnabled: false,
+  web2OriginEnabled: false,
 }
 
 const GlobalContext = createContext<ContextValue>({
@@ -92,6 +107,8 @@ const GlobalStateProvider = ({ children }: { children: React.ReactElement }): Re
         return { ...state, beeApiUrl: action.newValue }
       case 'BEE_DEBUG_API_URL_SAVE':
         return { ...state, beeDebugApiUrl: action.newValue }
+      case 'WEB2_ORIGIN_ENABLED_SAVE':
+        return { ...state, web2OriginEnabled: action.newValue }
       case 'GLOBAL_POSTAGE_BATCH_SAVE':
         return { ...state, postageBatchId: action.newValue }
       case 'GLOBAL_POSTAGE_BATCH_ENABLED_SAVE':
@@ -115,6 +132,11 @@ const GlobalStateProvider = ({ children }: { children: React.ReactElement }): Re
         uiStateDispatch({ type: 'BEE_DEBUG_API_URL_SAVE', newValue })
       }
     }
+    const web2OriginEnableListener = (newValue: boolean, oldValue: boolean) => {
+      if (newValue !== oldValue && newValue !== state.web2OriginEnabled) {
+        uiStateDispatch({ type: 'WEB2_ORIGIN_ENABLED_SAVE', newValue })
+      }
+    }
     // localstore changes effect back the handled state
     const globalPostageBatchListener = (newValue: string, oldValue: string) => {
       if (newValue !== oldValue && newValue !== state.postageBatchId) {
@@ -131,12 +153,14 @@ const GlobalStateProvider = ({ children }: { children: React.ReactElement }): Re
     storeObserver.addListener('beeDebugApiUrl', beeDebugApiUrlListener)
     storeObserver.addListener('globalPostageBatch', globalPostageBatchListener)
     storeObserver.addListener('globalPostageStampEnabled', globalPostageBatchEnabledListener)
+    storeObserver.addListener('web2OriginEnabled', web2OriginEnableListener)
 
     return () => {
       storeObserver.removeListener('beeApiUrl', beeApiUrlListener)
       storeObserver.removeListener('beeDebugApiUrl', beeDebugApiUrlListener)
       storeObserver.removeListener('globalPostageBatch', globalPostageBatchListener)
       storeObserver.removeListener('globalPostageStampEnabled', globalPostageBatchEnabledListener)
+      storeObserver.removeListener('web2OriginEnabled', web2OriginEnableListener)
     }
   }, [])
 
