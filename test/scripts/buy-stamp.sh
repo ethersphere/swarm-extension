470b012f014b0f37a3a26cebd1a32ca1e748a1be
Vladan <tomic.vladan.mail@gmail.com>
2022-06-01 17:25:54 +0200

    test: fix buy-stamp script

--- test/scripts/buy-stamp.sh
+++ test/scripts/buy-stamp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# sleep 20
 
 if [[ -z "${BEE_STAMP}" ]]; then
-  BEE_STAMP=$( curl -s -XPOST http://127.0.0.1:1635/stamps/1/20 | cut -c 13-76 )
+  BEE_STAMP=`curl -s -XPOST http://127.0.0.1:1635/stamps/1/20 | cut -c 13-76`
 fi
 
 echo $BEE_STAMP
