b17716e0c85832546539c94450d91fcdc19840b5
Vladan <tomic.vladan.mail@gmail.com>
2022-06-29 10:51:19 +0200

    chore: add missing definition

--- src/utils/message/web2-helper/web2-helper.message.ts
+++ src/utils/message/web2-helper/web2-helper.message.ts
@@ -2,4 +2,6 @@
 // Key: Message name; Parameter: payload data from sender side, Return Value: Feeder functions emit
 export interface IWeb2HelperMessage {
   beeApiUrl: () => Promise<string>
+
+  isBeeApiAvailable(): Promise<boolean>
 }
